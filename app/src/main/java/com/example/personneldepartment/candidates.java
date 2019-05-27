61020491b9ceacb24cf2bd3f3169ae188094fa44
fdee9 <f.destorone@yandex.ru>
2019-05-27 15:46:58 -0700

    UPD27/05/19

--- app/src/main/java/com/example/personneldepartment/candidates.java
+++ app/src/main/java/com/example/personneldepartment/candidates.java
@@ -1,19 +1,32 @@
 package com.example.personneldepartment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class candidates extends AppCompatActivity {
-private ListView candidates;
+    private ListView candidates;
+    private Button addcand;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidates);
         candidates = findViewById(R.id.emoloyees);
+        addcand = findViewById(R.id.addcand);
         String[] names = {"Кипелова Анна Викторовна", "Александров Александр Олегович"};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,names);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, names);
         candidates.setAdapter(arrayAdapter);
+        final View.OnClickListener btn_startbdListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(candidates.this, addcand.class));
+            }
+        };
+        addcand.setOnClickListener(btn_startbdListener);
     }
 }
--- app/src/main/java/com/example/personneldepartment/employees.java
+++ app/src/main/java/com/example/personneldepartment/employees.java
@@ -1,19 +1,32 @@
 package com.example.personneldepartment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class employees extends AppCompatActivity {
     private ListView employees;
+    private Button addemp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees);
         employees = findViewById(R.id.emoloyees);
+        addemp = findViewById(R.id.addemp);
         String[] names = {"Устинов Егор Викторович", "Шевченко Андрей Александрович"};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,names);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, names);
         employees.setAdapter(arrayAdapter);
+        final View.OnClickListener btn_startbdListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(employees.this, addemp.class));
+            }
+        };
+        addemp.setOnClickListener(btn_startbdListener);
     }
 }
